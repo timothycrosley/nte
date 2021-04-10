09cb12568a46814567a19b5e736dce170702e714
Timothy Crosley <timothy.crosley@gmail.com>
2021-04-10 15:02:21 -0700

    Use files instead of putting all notes in the same file

--- nte/cli.py
+++ nte/cli.py
@@ -5,48 +5,45 @@ from subprocess import call
 import typer
 
 NOTES_CONFIG_FILE = Path("~/.nte_config.json").expanduser()
-NOTES_CONFIG_DEFAULT = {"datafile": Path("~/.nte_data.json").expanduser()}
+NOTES_CONFIG_DEFAULT = {"notes_dir": Path("~/.ntes").expanduser()}
 NOTES_CONFIG = (
     json.loads(NOTES_CONFIG_FILE.read_text())
     if NOTES_CONFIG_FILE.is_file()
     else NOTES_CONFIG_DEFAULT
 )
+NOTES_CONFIG["notes_dir"].mkdir(parents=True, exist_ok=True)
+NOTE_PATH = NOTES_CONFIG["notes_dir"]
 
 app = typer.Typer()
-data = (
-    json.loads(NOTES_CONFIG["datafile"].read_text()) if NOTES_CONFIG["datafile"].is_file() else {}
-)
-
 
 def save():
     Path(NOTES_CONFIG["datafile"]).write_text(json.dumps(data))
 
 
-def ensure_value(key: str):
-    value = data.get(key, {"value": None})["value"]
-    if not value:
+def note_value(key: str) -> str:
+    note_file = NOTE_PATH / key
+    if not note_file.is_file():
         typer.echo(f"No value stored for {key}!")
         raise typer.Abort()
-    return value
+    return note_file.read_text()
 
 
-@app.command()
-def set(key: str, value: str):
-    existing_value = data.get(key, {"value": None})["value"]
-    if existing_value and not typer.confirm(f"Replace existing value for {key}: {existing_value}."):
+@app.command(name="set")
+def _set(key: str, value: str, overwrite: bool=False):
+    note_file = NOTE_PATH / key
+    if note_file.exists() and not (overwrite or typer.confirm(f"Replace existing note for {key}.")):
         raise typer.Abort()
-    data[key] = {"value": value}
-    save()
+    note_file.write_text(value)
 
 
 @app.command()
 def get(key: str):
-    typer.echo(ensure_value(key))
+    typer.echo(note_value(key))
 
 
 @app.command()
 def run(key: str):
-    call(ensure_value(key), shell=True)
+    call(note_value(key), shell=True)
 
 
 if __name__ == "__main__":
