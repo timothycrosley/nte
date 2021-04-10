27d6c419aec1f7f46bddd801e72381713eeb99da
Timothy Crosley <timothy.crosley@gmail.com>
2021-04-10 01:08:14 -0700

    Provide a way to run commands

--- nte/cli.py
+++ nte/cli.py
@@ -1,17 +1,33 @@
-import typer
 import json
 from pathlib import Path
+from subprocess import call
+
+import typer
 
 NOTES_CONFIG_FILE = Path("~/.nte_config.json").expanduser()
 NOTES_CONFIG_DEFAULT = {"datafile": Path("~/.nte_data.json").expanduser()}
-NOTES_CONFIG = json.loads(NOTES_CONFIG_FILE.read_text()) if NOTES_CONFIG_FILE.is_file() else NOTES_CONFIG_DEFAULT
+NOTES_CONFIG = (
+    json.loads(NOTES_CONFIG_FILE.read_text())
+    if NOTES_CONFIG_FILE.is_file()
+    else NOTES_CONFIG_DEFAULT
+)
 
 app = typer.Typer()
-data = json.loads(NOTES_CONFIG["datafile"].read_text()) if NOTES_CONFIG["datafile"].is_file() else {}
+data = (
+    json.loads(NOTES_CONFIG["datafile"].read_text()) if NOTES_CONFIG["datafile"].is_file() else {}
+)
 
 
 def save():
     Path(NOTES_CONFIG["datafile"]).write_text(json.dumps(data))
+
+
+def ensure_value(key: str):
+    value = data.get(key, {"value": None})["value"]
+    if not value:
+        typer.echo(f"No value stored for {key}!")
+        raise typer.Abort()
+    return value
 
 
 @app.command()
@@ -25,13 +41,12 @@ def set(key: str, value: str):
 
 @app.command()
 def get(key: str):
-    value = data.get(key, {"value": None})["value"]
-    if not value:
-        typer.echo(f"No value stored for {key}!")
-        return
-    typer.echo(value)
+    typer.echo(ensure_value(key))
 
 
+@app.command()
+def run(key: str):
+    call(ensure_value(key), shell=True)
 
 
 if __name__ == "__main__":
